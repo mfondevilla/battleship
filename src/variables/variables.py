ddb32b8198d03358774db7f370a854c9ba141116
mfondevilla <m_fondevilla@hotmail.com>
2026-03-29 22:55:27 +0200

    definicion variables

---
+++ src/variables/variables.py
@@ -0,0 +1,22 @@
+#variables/variables.py
+
+BOARD_SIZE = 10,
+WATER = "-"
+SHIP = "O"
+HIT = "X"
+MISS = "*"
+#validators
+VALID_LETTERS = "ABCDEFGHIJ"
+VALID_NUMBERS = list(range(1, BOARD_SIZE + 1))
+SHIPS = {
+    "length4": 4,
+    "length3": 3,
+    "length3": 3,
+    "length2": 2,
+    "length2": 2,
+    "length2": 2,
+    "length1": 1,
+    "length1": 1,
+    "length1": 1,
+    "length1": 1
+}
