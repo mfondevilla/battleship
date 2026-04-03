4e5dd20ce3c443ccdc22175123a729e350d067d2
acofra <ana.corrochano@gmail.com>
2026-04-03 09:40:17 +0100

    main.py done

---
+++ main.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+from src.clases.clases import Tablero, SalirJuego
+from src.functions.function import (
+    mostrar_menu,
+    turno_jugador,
+    turno_cpu,
+    check_winner,
+)
+
+def jugar():
+    print("Hola")
+    opcion_elegida = mostrar_menu()
+    jugador = Tablero("Jugador")
+    cpu = Tablero("CPU")
+    cpu.place_ships() # colocar los barcos random
+    jugador.place_ships()
+    
+    if opcion_elegida == '1':
+        while (True):
+            cpu.display_barcos() # test para disparar todos los barcos de cpu
+            print("Turno Jugador")
+
+            turno_jugador(cpu) # jugador selecciona coordenadas para atacar tablero de cpu
+            print("Tablero cpu")
+
+            cpu.display_disparos()
+            print("Turno CPU")
+
+            turno_cpu(jugador)
+            print("Tablero jugador")
+
+            jugador.display_barcos()
+
+            cpu_is_winner = check_winner(jugador)
+            jugador_is_winner = check_winner(cpu)
+
+            if (cpu_is_winner):
+                print("Ha ganado CPU !")
+
+                return False
+            elif(jugador_is_winner):  
+                print("Has ganado! Enhorabuena!")
+                return False
+
+
+    elif opcion_elegida == '3':
+        return # se sale del juego
+jugar()
